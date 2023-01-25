e7a44fb887f36a07bb69fa62e49fd2ffaa88191d
4dayman <109858025+4dayman@users.noreply.github.com>
2023-01-25 17:45:27 +0200

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -82,7 +82,15 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        selector: to.hash,
+        behavior: 'smooth',
+      }
+    }
+  }
 })
 
 export default router
