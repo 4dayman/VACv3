e75a39c66b549e9c20bf3b41b74479f594907833
4dayman <109858025+4dayman@users.noreply.github.com>
2023-01-25 16:07:11 +0200

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -81,8 +81,7 @@ const routes = [
 
 
 const router = createRouter({
-  // history: createWebHistory(process.env.BASE_URL),
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
