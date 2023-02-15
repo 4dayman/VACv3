fb71c207f9bedd238bf2c6d54b8da2769d2507b8
4dayman <109858025+4dayman@users.noreply.github.com>
2023-02-15 11:16:02 +0200

    commit

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="inventory_wrapper">
-        <div class="inventory">
-            <div class="inventory_sidebar">
+    <div class="catalog_wrapper">
+        <div class="catalog">
+            <div class="catalog_sidebar">
                 <h2>Detailed search</h2>
-                <div class="inventory_filters">
-                    <div class="inventory_filter" :class="open1 ? 'open1' : ''" >
+                <div class="catalog_filters">
+                    <div class="catalog_filter" :class="open1 ? 'open1' : ''" >
                         <div class="filter_title" @click="open1 = !open1">Make, Model</div>
                         <div class="filter_desc" :class="open1 ? 'open' : ''">
                             <div class="search_make">
@@ -17,7 +17,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="inventory_filter" :class="open2 ? 'open2' : ''" >
+                    <div class="catalog_filter" :class="open2 ? 'open2' : ''" >
                         <div class="filter_title" @click="open2 = !open2">Body type</div>
                         <div class="filter_desc" :class="open2 ? 'open' : ''">
                             <div class="checkbox">
@@ -71,7 +71,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="inventory_filter" :class="open3 ? 'open3' : ''" >
+                    <div class="catalog_filter" :class="open3 ? 'open3' : ''" >
                         <div class="filter_title" @click="open3 = !open3">Transmission</div>
                         <div class="filter_desc" :class="open3 ? 'open' : ''">
                             <div class="checkbox">
@@ -88,7 +88,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="inventory_filter" :class="open4 ? 'open4' : ''" >
+                    <div class="catalog_filter" :class="open4 ? 'open4' : ''" >
                         <div class="filter_title" @click="open4 = !open4">Price</div>
                         <div class="filter_desc" :class="open4 ? 'open' : ''">
                             <div class="price_value">
@@ -104,7 +104,7 @@
 
                         </div>
                     </div>
-                    <div class="inventory_filter" :class="open5 ? 'open5' : ''" >
+                    <div class="catalog_filter" :class="open5 ? 'open5' : ''" >
                         <div class="filter_title" @click="open5 = !open5">Year</div>
                         <div class="filter_desc" :class="open5 ? 'open' : ''">
                             <div class="year_value">
@@ -119,7 +119,7 @@
                             />
                         </div>
                     </div>
-                    <div class="inventory_filter" :class="open6 ? 'open6' : ''" >
+                    <div class="catalog_filter" :class="open6 ? 'open6' : ''" >
                         <div class="filter_title" @click="open6 = !open6">Kilometres</div>
                         <div class="filter_desc" :class="open6 ? 'open' : ''">
                             <div class="kilometres_value">
@@ -137,24 +137,49 @@
                     </div>
                 </div>
             </div>
+            <div class="catalog_mainbar">
+                <div class="mainbar_header">
+                    <div class="mainbar_header_left">
+                        <div class="main_searchbar">
+                            <input class="input search_input" type="text" placeholder="Find a dream car...">
+                            <img src="../assets/Share.svg" alt="">
+                        </div>
+                        
+                    </div>
+                    <div class="mainbar_header_right">
+                        <div class="main_right">
+                            <p class="sorted">Sorted by</p>
+                            <div class="catalog_filter recommendations" :class="open7 ? 'open7' : ''" >
+                                <div class="filter_title recommendations" @click="open7 = !open7">Recommendations</div>
+                                <div class="filter_desc" :class="open7 ? 'open' : ''">
+                                    <p>Newest inventory</p>
+                                    <p>Lowest price</p>
+                                    <p>Highest prices</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
 import Slider from '@vueform/slider'
-import "@vueform/slider/themes/default.css"
+import "@vueform/slider/themes/default.scss"
 export default {
     components: {
         Slider,
     },
     data() {
         return {
-            open1: true,
-            open2: true,
-            open3: true,
-            open4: true,
-            open5: true,
-            open6: true,
+            open1: false,
+            open2: false,
+            open3: false,
+            open4: false,
+            open5: false,
+            open6: false,
+            open7: false,
             price: [10000, 100000],
             year: [2000, 2005],
             kilometres: 100000,
@@ -210,16 +235,27 @@ box-shadow: none;
 // ------end of slider-------
 
 
-.inventory_wrapper{
+.catalog_wrapper{
     max-width: 1230px;
     margin: 0 auto;
     padding: 0 24px;
+    @media (min-width: 1278px) {
+        padding: 0;
+    }
     @media (max-width: 460px) {
         padding: 0 20px;
     }
 }
-.inventory_sidebar {
+.catalog {
+    display: grid;
+    grid-template-columns: 300px 1fr;
+    column-gap: 40px;
+}
+.catalog_sidebar, 
+.catalog_mainbar {
     margin-top: 49px;
+}
+.catalog_sidebar {
     width: 300px;
     height: 100%;
     h2{
@@ -231,12 +267,53 @@ box-shadow: none;
         margin-bottom: 31px;
     }
 }
-.inventory_filters {
+.catalog_mainbar {
     width: 100%;
 }
-.inventory_filter {
+.main_searchbar {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+}
+.main_right {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    .sorted {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 180%;
+        letter-spacing: 0.02em;
+        color: #41456B;
+    }
+}
+.mainbar_header {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.search_input {
+    max-width: 350px;
+}
+.catalog_filters {
+    width: 100%;
+}
+.catalog_filter {
     margin-bottom: 10px;
     border: 1px solid #D7D7D7;
+    position: relative;
+}
+.catalog_filter.recommendations{
+    p{
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 159%;
+        &:not(:last-child){
+            padding-bottom: 10px;
+        }
+    }
+    width: 237px;
 }
 .filter_title{
     padding: 10px 20px 10px 20px;
@@ -262,32 +339,34 @@ box-shadow: none;
         background-repeat: no-repeat;
         transition: all 0.2s linear;
     }
-    @media (max-width: 450px) {
-        font-size: 18px;
-        padding: 20px 59px 20px 20px;
-    }
 }
-.inventory_filter.open1 .filter_title::after{
+.catalog_filter.open1 .filter_title::after{
     transform: translateY(-50%) rotate(180deg);
 }
-.inventory_filter.open2 .filter_title::after{
+.catalog_filter.open2 .filter_title::after{
     transform: translateY(-50%) rotate(180deg);
 }
-.inventory_filter.open3 .filter_title::after{
+.catalog_filter.open3 .filter_title::after{
     transform: translateY(-50%) rotate(180deg);
 }
-.inventory_filter.open4 .filter_title::after{
+.catalog_filter.open4 .filter_title::after{
     transform: translateY(-50%) rotate(180deg);
 }
-.inventory_filter.open5 .filter_title::after{
+.catalog_filter.open5 .filter_title::after{
     transform: translateY(-50%) rotate(180deg);
 }
-.inventory_filter.open6 .filter_title::after{
+.catalog_filter.open6 .filter_title::after{
     transform: translateY(-50%) rotate(180deg);
+}
+.catalog_filter.open7 .filter_title::after{
+    transform: translateY(-50%) rotate(180deg);
+    margin-top: 5px;
+}
+.catalog_filter.open7 .filter_title{
+    padding-bottom: 0;
 }
 .filter_desc {
     padding: 0 20px;
-    padding-top: 8px;
     text-align: left;
     font-weight: 400;
     font-size: 16px;
@@ -302,6 +381,7 @@ box-shadow: none;
         opacity:1;
         max-height: 1000px;
         padding-bottom: 20px;
+        padding-top: 8px;
         @media (max-width: 450px) {
             padding-bottom: 20px;
         }
